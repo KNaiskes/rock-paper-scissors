a7579cb139acaf6f8ecdcbda46e8e2ecc3152173
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-05-13 22:52:09 +0300

    Add valid options list

--- src/player.py
+++ src/player.py
@@ -1,14 +1,15 @@
 class Player:
+    valid_guesses = ['rock', 'paper', 'scissors']
+
     def __init__(self, player_name):
         self.player_name = player_name
         self.score = 0
         self.guess = ''
 
     def get_player_guess(self, guess):
-        valid_guesses = ['rock', 'paper', 'scissors']
 
-        while(guess not in valid_guesses):
-            print(f'Valid options are: {", ".join(valid_guesses)}')
+        while(guess not in self.valid_guesses):
+            print(f'Valid options are: {", ".join(self.valid_guesses)}')
             guess = input().lower()
 
         self.guess = guess.lower()
