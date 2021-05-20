f10b6867c2ad0140bd00cdb80170499c59851e6c
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-05-20 13:49:48 +0300

    Add guess field in Computer class

--- src/computer.py
+++ src/computer.py
@@ -4,6 +4,7 @@ class Computer(Player):
     def __init__(self):
         self.player_name = 'Computer'
         self.score = 0
+        self.guess = ''
 
     def get_guess(self):
         import random
